2b19587341fd1361317ce6f59f5c5a280a137190
Pratham Dongre <124212024+Mafuyuchan@users.noreply.github.com>
2025-11-20 15:43:47 +0900

    Update resnet50_transfer.py

--- models/resnet50_transfer.py
+++ models/resnet50_transfer.py
@@ -4,25 +4,36 @@ from torchvision import models
 
 
 class ResNet50Transfer(nn.Module):
-def __init__(self, num_classes=4, pretrained=True, dropout=0.5):
-super().__init__()
-resnet = models.resnet50(pretrained=pretrained)
-# Freeze early layers (optional)
-for name, param in resnet.named_parameters():
-if "layer4" not in name:
-param.requires_grad = False
+    def __init__(self, num_classes=4, pretrained=True, dropout=0.5):
+        super().__init__()
 
+        # Load ResNet50
+        # TorchVision >= 0.13 uses "weights" instead of pretrained=True
+        if pretrained:
+            weights = models.ResNet50_Weights.DEFAULT
+            resnet = models.resnet50(weights=weights)
+        else:
+            resnet = models.resnet50(weights=None)
 
-in_features = resnet.fc.in_features
-resnet.fc = nn.Sequential(
-nn.Dropout(dropout),
-nn.Linear(in_features, 512),
-nn.ReLU(inplace=True),
-nn.Dropout(dropout),
-nn.Linear(512, num_classes)
-)
-self.model = resnet
+        # Freeze all layers except layer4 + fc
+        for name, param in resnet.named_parameters():
+            if not name.startswith("layer4") and not name.startswith("fc"):
+                param.requires_grad = False
 
+        # Replace FC layer
+        in_features = resnet.fc.in_features
+        resnet.fc = nn.Sequential(
+            nn.Dropout(dropout),
+            nn.Linear(in_features, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(dropout),
+            nn.Linear(512, num_classes)
+        )
 
-def forward(self, x):
-return self.model(x)
+        self.model = resnet
+
+        # You will use this in GradCAM(model, model.target_layer)
+        self.target_layer = self.model.layer4[-1]
+
+    def forward(self, x):
+        return self.model(x)
